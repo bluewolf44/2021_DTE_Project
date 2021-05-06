98ee1e0a05fb6785f4e24601143ce1a946a7c307
Blue <danielwest618@gmail.com>
2021-05-07 10:00:56 +1200

    finish animions

--- Project/Scripts/Player.gd
+++ Project/Scripts/Player.gd
@@ -8,4 +8,9 @@ func _process(delta):
 		Input.get_action_strength("Move_Down")-Input.get_action_strength("Move_Up")
 		)
 	move_and_slide(move.normalized()*speed)
-	
+	if move:
+		$AnimationTree.get("parameters/playback").travel("Run")
+		$AnimationTree.set("parameters/Stand/blend_position",move)
+		$AnimationTree.set("parameters/Run/blend_position",move)
+	else:
+		$AnimationTree.get("parameters/playback").travel("Stand")
